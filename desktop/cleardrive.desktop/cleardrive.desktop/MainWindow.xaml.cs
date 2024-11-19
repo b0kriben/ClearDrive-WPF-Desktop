bae5578e38d90eb6c0bd75c931b0cbd5cca8fcb0
Benedek Krisztin <b0kriben@vasvari.org>
2024-11-19 19:42:58 +0100

    hibák javitása: nincs kész

--- desktop/cleardrive.desktop/cleardrive.desktop/MainWindow.xaml.cs
+++ desktop/cleardrive.desktop/cleardrive.desktop/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//using cleardrive.viewmodel;
+//using cleardrive.desktop.viewmodel;
 //using System.Text.Json;
 
 namespace cleardrive.desktop
