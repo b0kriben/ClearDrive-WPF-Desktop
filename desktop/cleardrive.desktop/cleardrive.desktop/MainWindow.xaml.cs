bdc791723afc8115457f1afa3320665fe915423d
Benedek Krisztin <b0kriben@vasvari.org>
2024-11-20 19:04:02 +0100

    Hibajavítások: nem sikerültek

--- desktop/cleardrive.desktop/cleardrive.desktop/MainWindow.xaml.cs
+++ desktop/cleardrive.desktop/cleardrive.desktop/MainWindow.xaml.cs
@@ -8,8 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//using cleardrive.desktop.viewmodel;
-//using System.Text.Json;
+using cleardrive.desktop.viewmodel;
+using Models;
 
 namespace cleardrive.desktop
 {
@@ -18,80 +18,10 @@ namespace cleardrive.desktop
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        //private static readonly HttpClient client = new HttpClient();
-        //private readonly HttpClient _httpClient;
-
         public MainWindow()
         {
             InitializeComponent();
             //this.DataContext = new MainViewModel();
-            //_httpClient = new HttpClient();
         }
-
-        /*private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var response = await GetItemsFromApi();
-            MessageBox.Show(response);
-        }
-
-        private async Task<string> GetItemsFromApi()
-        {
-            try
-            {
-                // A backend API elérési URL-je
-                string apiUrl = "http://localhost:5000/api/items";
-
-                // GET kérés küldése
-                var response = await client.GetStringAsync(apiUrl);
-                return response;
-            }
-            catch (HttpRequestException ex)
-            {
-                return "Hiba történt: " + ex.Message;
-            }
-        }*/
-
-
-
-
-
-        /*private async Task GetDataFromApi()
-        {
-            try
-            {
-                string apiUrl = "https://your-api-endpoint.com/data";
-
-                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    // API válaszának beolvasása JSON formátumban
-                    string responseData = await response.Content.ReadAsStringAsync();
-
-                    // A JSON adat deszerializálása (feltételezzük, hogy van egy Data típusú osztály)
-                    var data = JsonConvert.DeserializeObject<MyDataType>(responseData);
-
-                    // UI frissítése a lekért adatokkal
-                    Dispatcher.Invoke(() =>
-                    {
-                        // Például egy TextBox frissítése
-                        myTextBox.Text = data.SomeProperty;
-                    });
-                }
-                else
-                {
-                    MessageBox.Show("Hiba történt a kérés feldolgozása közben.");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Hiba: {ex.Message}");
-            }
-        }
-
-        private async void OnButtonClick(object sender, RoutedEventArgs e)
-        {
-            await GetDataFromApi();
-        }*/
     }
 }
